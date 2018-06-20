0b33dd7bf8584b5503a445163ad9e5a22adb18a4
Michael Cassidy <mikecassidy0000@gmail.com>
2018-06-20 19:54:31 -0400

    prepare for heroku deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://mike:mike@ds141242.mlab.com:41242/bentennas",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
